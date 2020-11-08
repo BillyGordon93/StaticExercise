6427cae6f0e6fe301ad79e3c6dc60f49b95fa3d5
Billy Gordon <Billywgordon93@gmail.com>
2020-11-08 12:12:13 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me the temp you wish to convert from Fahrenheit to Celsius.");
+            double numOne = double.Parse(Console.ReadLine());
+
+            double cel = TempConverter.FahrenheitToCelsius(numOne);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Celsius :  {cel, 0:N2}");
+            Console.ResetColor();
+
+            Console.WriteLine("Please give me a temp you wish to convert from Celsius to Fahrenheit");
+            double numTwo = double.Parse(Console.ReadLine());
+
+
+            double fahrenheit = TempConverter.CelsiusToFahren(numTwo);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Fahrenheit : {fahrenheit,0:N2}");
+            Console.ResetColor();
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //static classes can only contain static numbers
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            //what is my goal here?
+            //do some math to convert fahrenheit to celsius
+            return (fahrenheit - 32) / 1.8;//this conversion will return a double
+
+        }
+        public static double CelsiusToFahren(double celsius)
+        {
+            //whats my goal here?
+            //convert celsius tofahrenheit
+            return (celsius * 9) / 5 + 32;
+
+        }
+    }
+}
